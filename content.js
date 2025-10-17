f7906a51be97ea3579c9ea9e4af4fb387f0a8153
ig <gaibullaeviz@gmail.com>
2025-10-18 01:16:24 +0300

    feature: add modes

--- content.js
+++ content.js
@@ -1,56 +1,98 @@
 (function () {
-    let selecting = false;
-    let highlightedElement = null;
-    let interval = null;
+  let selecting = false;
+  let highlightedElement = null;
+  let intervalId = null;
 
-    const style = document.createElement("style");
-    style.textContent = `
+  const style = document.createElement("style");
+  style.textContent = `
     ._highlighted-element {
       outline: 2px solid red !important;
       cursor: crosshair !important;
       background-color: brown;
     }
+    ._invisible {
+        visibility: hidden !important;
+    }
+    ._display_none {
+        display: none !important;
+    }
   `;
-    document.head.appendChild(style);
+  document.head.appendChild(style);
 
-    document.addEventListener("activateElementPicker", () => {
-        selecting = !selecting;
-        if (selecting) {
-            document.body.style.cursor = "crosshair";
-            alert("Select mode enabled - point and click to delete.");
-        } else {
-            document.body.style.cursor = "default";
-            if (highlightedElement) highlightedElement.classList.remove("_highlighted-element");
-            if (interval) clearInterval(interval);
-        }
+  document.addEventListener("activateElementPicker", () => {
+    selecting = !selecting;
+    if (selecting) {
+      document.body.style.cursor = "crosshair";
+      alert("Select mode enabled - point and click to delete.");
+    } else {
+      document.body.style.cursor = "default";
+      if (highlightedElement)
+        highlightedElement.classList.remove("_highlighted-element");
+      if (intervalId) clearInterval(intervalId);
+    }
+  });
+
+  document.addEventListener("mouseover", (e) => {
+    if (!selecting) return;
+    if (highlightedElement)
+      highlightedElement.classList.remove("_highlighted-element");
+    highlightedElement = e.target;
+    highlightedElement.classList.add("_highlighted-element");
+  });
+
+  document.addEventListener("mouseout", (e) => {
+    if (!selecting) return;
+    if (e.target.classList.contains("_highlighted-element")) {
+      e.target.classList.remove("_highlighted-element");
+    }
+  });
+
+  document.addEventListener("click", async (e) => {
+    if (!selecting) return;
+    e.preventDefault();
+    e.stopPropagation();
+    selecting = false;
+    document.body.style.cursor = "default";
+    e.target.classList.remove("_highlighted-element");
+    const { interval } = await chrome.storage.local.get(["interval"]);
+    const { mode } = await chrome.storage.local.get(["mode"]);
+    console.log("CONFIG", {
+      interval,
+      mode,
     });
-
-    document.addEventListener("mouseover", (e) => {
-        if (!selecting) return;
-        if (highlightedElement) highlightedElement.classList.remove("_highlighted-element");
-        highlightedElement = e.target;
-        highlightedElement.classList.add("_highlighted-element");
-    });
-
-    document.addEventListener("mouseout", (e) => {
-        if (!selecting) return;
-        if (e.target.classList.contains("_highlighted-element")) {
-            e.target.classList.remove("_highlighted-element");
-        }
-    });
-
-    document.addEventListener("click", (e) => {
-        if (!selecting) return;
-        e.preventDefault();
-        e.stopPropagation();
-        selecting = false;
-        document.body.style.cursor = "default";
+    switch (mode) {
+      case "delete":
         e.target.remove();
-        if (e.target.classList[0]) {
-            console.log("The element with the class - " + e.target.classList[0] + " is removed");
-            interval = setInterval(() => {
-                document.body.querySelector(`.${e.target.classList[0]}`)?.remove();
-            }, 10);
-        }
-    });
+        break;
+      case "invisible":
+        e.target.classList.add("_invisible");
+        break;
+      case "display_none":
+        e.target.classList.add("_display_none");
+        break;
+    }
+    if (interval && e.target.classList[0]) {
+      switch (mode) {
+        case "delete":
+          intervalId = setInterval(() => {
+            document.body.querySelector(`.${e.target.classList[0]}`)?.remove();
+          }, 10);
+          break;
+        case "invisible":
+          intervalId = setInterval(() => {
+            document.body
+              .querySelector(`.${e.target.classList[0]}`)
+              ?.classList?.add("_invisible");
+          }, 10);
+          break;
+        case "display_none":
+          intervalId = setInterval(() => {
+            document.body
+              .querySelector(`.${e.target.classList[0]}`)
+              ?.classList?.add("_display_none");
+          }, 10);
+          break;
+      }
+    }
+  });
 })();
--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,32 @@
+const checkbox = document.getElementById("intervalCheckbox");
+const select = document.getElementById("modeSelect");
+
+chrome.storage.local.get(["interval"], (result) => {
+  checkbox.checked = result.interval || false;
+});
+
+chrome.storage.local.get(["mode"], (result) => {
+  select.value = result.mode || "delete";
+});
+
 document.getElementById("start").addEventListener("click", async () => {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: () => {
-            document.dispatchEvent(new CustomEvent("activateElementPicker"));
-        }
-    });
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      document.dispatchEvent(new CustomEvent("activateElementPicker"));
+    },
+  });
+});
+
+checkbox.addEventListener("change", async () => {
+  chrome.storage.local.set({ interval: checkbox.checked }, () => {
+    console.log("Set interval mode:", checkbox.checked);
+  });
+});
+
+select.addEventListener("change", async () => {
+  chrome.storage.local.set({ mode: select.value }, () => {
+    console.log("Set mode:", select.value);
+  });
 });
